76e2d61491c6437edbaedce260555a8cb9329ece
Zyko <micaiahejardin@gmail.com>
2026-02-01 16:13:14 -0600

    Update math.js

--- js/math.js
+++ js/math.js
@@ -1,1 +1,13 @@
+// math.js
+
+export const Mat4 = {
+    identity() {
+        return new Float32Array([
+            1,0,0,0,
+            0,1,0,0,
+            0,0,1,0,
+            0,0,0,1
+        ]);
+    }
+};
 
