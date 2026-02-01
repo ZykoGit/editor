5f8965035a32d8b753f10a263b1d371f77c1a5e2
Zyko <micaiahejardin@gmail.com>
2026-02-01 16:12:21 -0600

    Update cube.js

--- js/cube.js
+++ js/cube.js
@@ -1,1 +1,23 @@
+// cube.js
+
+import { Mat4 } from "./math.js";
+
+export class Cube {
+    constructor() {
+        this.modelMatrix = Mat4.identity();
+
+        this.vertices = new Float32Array([
+            // x, y, z,   u, v
+            -1, -1,  1,  0, 0,
+             1, -1,  1,  1, 0,
+             1,  1,  1,  1, 1,
+            -1,  1,  1,  0, 1,
+        ]);
+
+        this.indices = new Uint16Array([
+            0, 1, 2,
+            2, 3, 0
+        ]);
+    }
+}
 
