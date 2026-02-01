d35a594d3aa77bc59a82d202af69ffa0fe3d7350
Zyko <micaiahejardin@gmail.com>
2026-02-01 16:14:11 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
+// main.js
+
+import { Renderer } from "./renderer.js";
+import { Cube } from "./cube.js";
+
+let cube;
+
+window.onload = () => {
+    const canvas = document.getElementById("glcanvas");
+    Renderer.init(canvas);
+
+    cube = new Cube();
+
+    requestAnimationFrame(loop);
+};
+
+function loop() {
+    const gl = Renderer.gl;
+
+    gl.clearColor(0.1, 0.1, 0.1, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    // TODO: draw cube
+
+    requestAnimationFrame(loop);
+}
 
