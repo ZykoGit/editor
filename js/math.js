4df877a91da3e8865f8866f7e7757be05c492002
Zyko <micaiahejardin@gmail.com>
2026-02-01 16:30:38 -0600

    Update math.js

--- js/math.js
+++ js/math.js
@@ -10,48 +10,68 @@ export const Mat4 = {
         ]);
     },
 
-    perspective(fov, aspect, near, far) {
-        const f = 1.0 / Math.tan(fov / 2);
-        const nf = 1 / (near - far);
+    multiply(a, b) {
+        const out = new Float32Array(16);
 
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 4; j++) {
+                out[i*4 + j] =
+                    a[i*4 + 0] * b[0*4 + j] +
+                    a[i*4 + 1] * b[1*4 + j] +
+                    a[i*4 + 2] * b[2*4 + j] +
+                    a[i*4 + 3] * b[3*4 + j];
+            }
+        }
+
+        return out;
+    },
+
+    translation(x, y, z) {
+        const m = Mat4.identity();
+        m[12] = x;
+        m[13] = y;
+        m[14] = z;
+        return m;
+    },
+
+    scale(x, y, z) {
+        const m = Mat4.identity();
+        m[0] = x;
+        m[5] = y;
+        m[10] = z;
+        return m;
+    },
+
+    rotationX(rad) {
+        const c = Math.cos(rad);
+        const s = Math.sin(rad);
         return new Float32Array([
-            f/aspect, 0, 0, 0,
-            0, f, 0, 0,
-            0, 0, (far+near)*nf, -1,
-            0, 0, (2*far*near)*nf, 0
+            1,0,0,0,
+            0,c,-s,0,
+            0,s,c,0,
+            0,0,0,1
         ]);
     },
 
-    lookAt(eye, target, up) {
-        const ex = eye[0], ey = eye[1], ez = eye[2];
-        const tx = target[0], ty = target[1], tz = target[2];
-
-        let zx = ex - tx;
-        let zy = ey - ty;
-        let zz = ez - tz;
-
-        const zLen = Math.hypot(zx, zy, zz);
-        zx /= zLen; zy /= zLen; zz /= zLen;
-
-        let xx = up[1]*zz - up[2]*zy;
-        let xy = up[2]*zx - up[0]*zz;
-        let xz = up[0]*zy - up[1]*zx;
-
-        const xLen = Math.hypot(xx, xy, xz);
-        xx /= xLen; xy /= xLen; xz /= xLen;
-
-        let yx = zy*xz - zz*xy;
-        let yy = zz*xx - zx*xz;
-        let yz = zx*xy - zy*xx;
-
+    rotationY(rad) {
+        const c = Math.cos(rad);
+        const s = Math.sin(rad);
         return new Float32Array([
-            xx, yx, zx, 0,
-            xy, yy, zy, 0,
-            xz, yz, zz, 0,
-            -(xx*ex + xy*ey + xz*ez),
-            -(yx*ex + yy*ey + yz*ez),
-            -(zx*ex + zy*ey + zz*ez),
-            1
+            c,0,s,0,
+            0,1,0,0,
+            -s,0,c,0,
+            0,0,0,1
+        ]);
+    },
+
+    rotationZ(rad) {
+        const c = Math.cos(rad);
+        const s = Math.sin(rad);
+        return new Float32Array([
+            c,-s,0,0,
+            s,c,0,0,
+            0,0,1,0,
+            0,0,0,1
         ]);
     }
 };
