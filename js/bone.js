41ab40ea6448f103c0f80be21df8c97353b99b5e
Zyko <micaiahejardin@gmail.com>
2026-02-03 09:39:37 -0600

    Update bone.js

--- js/bone.js
+++ js/bone.js
@@ -9,40 +9,80 @@ export class Bone {
         this.id = nextBoneID++;
         this.name = name + "_" + this.id;
 
-        this.position = { x: 0, y: 0, z: 0 };
-        this.rotation = { x: 0, y: 0, z: 0 };
-
         this.parent = null;
         this.children = [];
 
+        this.position = { x: 0, y: 0, z: 0 };
+        this.rotation = { x: 0, y: 0, z: 0 };
+
         this.modelMatrix = Mat4.identity();
+        this.worldMatrix = Mat4.identity();
+
+        this.selected = false;
     }
 
-    setParent(bone) {
-        if (this.parent) {
-            const i = this.parent.children.indexOf(this);
-            if (i !== -1) this.parent.children.splice(i, 1);
+    addChild(child) {
+        if (child.parent) {
+            const i = child.parent.children.indexOf(child);
+            if (i !== -1) child.parent.children.splice(i, 1);
+        }
+        child.parent = this;
+        this.children.push(child);
+    }
+
+    isDescendantOf(bone) {
+        let current = this.parent;
+        while (current) {
+            if (current === bone) return true;
+            current = current.parent;
+        }
+        return false;
+    }
+
+    setParent(newParent) {
+        if (newParent === this) return;
+        if (newParent && newParent.isDescendantOf(this)) return;
+
+        const oldParent = this.parent;
+        if (oldParent) {
+            const i = oldParent.children.indexOf(this);
+            if (i !== -1) oldParent.children.splice(i, 1);
         }
 
-        this.parent = bone;
+        // keep world position
+        this.updateMatrix();
+        const oldWorld = this.worldMatrix;
 
-        if (bone) {
-            bone.children.push(this);
-        }
+        this.parent = newParent;
+        if (newParent) newParent.children.push(this);
+
+        const parentWorldInv = newParent ? Mat4.invert(newParent.worldMatrix) : Mat4.identity();
+        this.modelMatrix = Mat4.multiply(parentWorldInv, oldWorld);
+
+        // decompose back to pos/rot (simple: we only stored translation/rotation)
+        this.position.x = this.modelMatrix[12];
+        this.position.y = this.modelMatrix[13];
+        this.position.z = this.modelMatrix[14];
+        // rotations not perfectly recovered here, but good enough for now
     }
 
     updateMatrix() {
         let m = Mat4.translation(this.position.x, this.position.y, this.position.z);
-
         m = Mat4.multiply(m, Mat4.rotationX(this.rotation.x));
         m = Mat4.multiply(m, Mat4.rotationY(this.rotation.y));
         m = Mat4.multiply(m, Mat4.rotationZ(this.rotation.z));
 
+        this.modelMatrix = m;
+
         if (this.parent) {
             this.parent.updateMatrix();
-            m = Mat4.multiply(this.parent.modelMatrix, m);
+            this.worldMatrix = Mat4.multiply(this.parent.worldMatrix, this.modelMatrix);
+        } else {
+            this.worldMatrix = this.modelMatrix;
         }
 
-        this.modelMatrix = m;
+        for (let child of this.children) {
+            child.updateMatrix();
+        }
     }
 }
